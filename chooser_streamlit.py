8c272ab1e5661994c93646568bef248597c92be5
Winston Chu <wtchu8@gmail.com>
2024-07-13 23:03:39 -0400

    change msg

--- chooser_streamlit.py
+++ chooser_streamlit.py
@@ -28,7 +28,7 @@ def run_streamlit():
         dr.roll_dice()
 
     if first_roll:
-        st.write(f"This is my command: {dr.result}")
+        st.write(f"This is your fate: {dr.result}")
 
 
 class DiceRoller:
