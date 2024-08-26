a0b663c1d53633f51b24de59820d61a74ba2b77c
Winston Chu <wtchu8@gmail.com>
2024-08-26 00:42:04 -0400

    init custom

--- chooser_streamlit.py
+++ chooser_streamlit.py
@@ -14,26 +14,29 @@ def run_streamlit():
 
     dr = DiceRoller()
 
-    dr.mode = st.selectbox("Pick a Mode", dr.get_mode_dict())
+    dr.mode = st.selectbox("Pick a Mode", dr.get_mode_list())
 
     dr.set_options_from_mode()
 
-    opt_str = dr.get_option_list_string()
+    if len(dr.options) > 0:
+        opt_str = dr.get_option_list_string()
 
-    st.write(opt_str)
+        st.write(opt_str)
 
-    if st.button("Roll"):
-        # first_roll=True
-        dr.roll_dice()
+        if st.button("Roll"):
+            # first_roll=True
+            dr.roll_dice()
 
-    # if first_roll:
-        st.write(f"This is your fate: {dr.result}")
+        # if first_roll:
+            st.write(f"This is your fate: {dr.result}")
 
 
 class DiceRoller:
     def __init__(self, mode="yn"):
+        self.mode_list = ["Yes or No", "Games", "Die", "Custom"]
         self.mode=mode
-        self.set_options_from_mode()
+        self.options=[]
+        # self.set_options_from_mode()
         self.result = None
 
     def roll_dice(self):
@@ -41,23 +44,22 @@ class DiceRoller:
         return self.result
 
     def set_options_from_mode(self):
-        if self.mode in self.get_mode_dict():
-            self.options=self.get_mode_dict()[self.mode]
+        
+        if self.mode == "Yes or No":
+            self.options = ["Yes", "No"]
+        elif self.mode == "Games":
+            n_players = st.slider("Number of Players", min_value=1, max_value=14, value=5)
+            self.options = self.get_vgame_options(n_players)
+        elif self.mode == "Die":
+            n_sided_dice = st.slider("Number of sides of die", 1, 20)
+            self.options = list(range(1,n_sided_dice+1))
+        elif self.mode == "Custom":
+            self.options = self.get_custom_options()
         else:
-            self.options=[]
+            raise ValueError(f"Invalid mode: {self.mode}")
 
-    def get_mode_dict(self):
-        vgame_modes = {f"{n} Player Games":self.get_vgame_options(n) for n in range(6,1,-1)}
-        dice_modes = {f"{n}-sided Dice": list(range(1,n+1)) for n in range(3,21)}
-        mode_dict = {
-            "Yes or No": ["Yes", "No"],
-            **vgame_modes,
-            **dice_modes
-        }
-        return mode_dict
-
-    def get_modes(self):
-        return list(self.get_mode_dict().keys())
+    def get_mode_list(self):
+        return self.mode_list
 
     def append_to_options(self, item):
         self.options.append(item)
@@ -86,6 +88,30 @@ class DiceRoller:
         with open('game_max.json','rt', encoding='utf-8') as f:
             out_dict = json.load(f)
         return out_dict
+
+    def get_custom_options(self):
+
+        option=st.text_input("Add an option")
+
+        # if st.button("Add"):
+        if option:
+            st.session_state["option_list"].append(option)
+
+        if "option_list" not in st.session_state:
+            st.session_state["option_list"] = []
+
+        for i, option in enumerate(st.session_state["option_list"]):
+            st.write(f"{i}. {option}")
+
+        for i, option in enumerate(st.session_state["option_list"]):
+            st.checkbox(option, value=True, key=i, on_change=st.session_state["option_list"].remove(option))
+                
+
+        if st.button("Finished"):
+            return st.session_state["option_list"]
+
+        return []
+
 
 if __name__ == "__main__":
     main()
